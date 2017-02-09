24de5b173102c88ac2b1ad30555bd059bdb623df
이기문 사원 신기술개발팀 (gimunlee@lgcns.com <79982@CORP.LGCNS.COM>
2017-02-09 16:54:34 +0900

    change the listening port to 3003

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", function(req, res) {
     res.send("hello");
     console.log("request approved");
 });
-app.listen("80",function() {
+app.listen("3003",function() {
     console.log("listening");
 });
