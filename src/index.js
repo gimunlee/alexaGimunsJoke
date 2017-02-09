b8e10831df3084804877bd8b1f1f3237fe26e8b4
이기문 사원 신기술개발팀 (gimunlee@lgcns.com <79982@CORP.LGCNS.COM>
2017-02-09 17:46:21 +0900

    tutorial

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// var express = require('express');
+// var app = express();
+
+// app.get("/", function(req, res) {
+//     res.send("hello");
+//     console.log("request approved");
+// });
+// app.listen(process.env.PORT || "3003",function() {
+//     console.log("listening");
+// });
+
+console.log("first of indx.js");
+
+var Alexa = require('alexa-sdk');
+
+
+var handlers = {
+    "Hello": function() {
+        this.emit(':tell', "Hello world");
+    },
+    "AMAZON.HelpIntent": function() {
+        this.emit(':tell', "May I help you?");
+    }
+};
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+}
